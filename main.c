5519c88be6eab2a4a94d54f99d8c8ef973998bfa
David1845 <39275071+David1845@users.noreply.github.com>
2019-09-29 14:52:25 -0500

    Add files via upload
    thread safe

--- main.c
+++ main.c
@@ -16,13 +16,17 @@
 #define NumPlayers 2
 #define Handset 2
 #define cardsDelt 3
-char PokerPlayers[10][20];
-int skipcards;
-char pokerCards[52][7];
-int randomNumber[500];
-char flop[50];
-char AutoSetHands[32];
-int randomNumbers2[500];
+
+typedef struct pokerUtil{
+    char PokerPlayers[10][20];
+    int skipcards;
+    char pokerCards[52][7];
+    int randomNumber[500];
+    char flop[50];
+    char AutoSetHands[32];
+    int randomNumbers2[500];
+}pokerutil;
+
 typedef struct winningHand{
     int pair;
     int kicker;
@@ -51,51 +55,50 @@ typedef struct hands{
     struct winningHand besthand;
 }hands[26];
 
-struct hands AllHands[26];
 
-void displayFlop(){
+void displayFlop(pokerutil * util){
     int counter=0;
     for(int i=NumPlayers*2;i<NumPlayers*2+3;i++){
-        printf("%s ",pokerCards[i]);
+        printf("%s ",util->pokerCards[i]);
         counter++;
     }
     printf("\n");
 }
-void displaycards(){
+void displaycards(pokerutil * util){
     int counter=0;
     for(int i=0;i<52;i++){
-        printf("%s ",pokerCards[i]);
+        printf("%s ",util->pokerCards[i]);
         counter++;
         if(counter%13==0)
             printf("\n");
     }
     printf("\n");
 }
-void showAllHands(){
+void showAllHands(hands * AllHands){
     for(int i=0;i<Handset;i++){
         for(int j=0;j<2;j++)
-            printf("%s",AllHands[i].hand[j]);
+            printf("%s",AllHands[i]->hand[j]);
     }
 }
-void replace(int this, int withThat){
+void replace(int this, int withThat,pokerutil * util){
     char tempPokerCards[1][7];
-    memcpy(tempPokerCards[0], pokerCards[withThat], sizeof(pokerCards[0]));
-    memcpy(pokerCards[withThat], pokerCards[this], sizeof(pokerCards[0]));
-    memcpy(pokerCards[this], tempPokerCards[0], sizeof(pokerCards[0]));
+    memcpy(tempPokerCards[0], util->pokerCards[withThat], sizeof(util->pokerCards[0]));
+    memcpy(util->pokerCards[withThat], util->pokerCards[this], sizeof(util->pokerCards[0]));
+    memcpy(util->pokerCards[this], tempPokerCards[0], sizeof(util->pokerCards[0]));
 }
 
-void higherOrderFirst(){
+void higherOrderFirst(struct hands * AllHands){
     for(int i=0;i<Handset;i++){
-            if(AllHands[i].results[0]->Strength < AllHands[i].results[1]->Strength){
-                char tempPokerCards[1][20];
-    memcpy(tempPokerCards[0], AllHands[i].hand[0], sizeof(AllHands[i].hand[0]));
-    memcpy(AllHands[i].hand[0], AllHands[i].hand[1], sizeof(AllHands[i].hand[0]));
-    memcpy(AllHands[i].hand[1], tempPokerCards[0], sizeof(AllHands[i].hand[0]));
-            }
+        if(AllHands[i].results[0]->Strength < AllHands[i].results[1]->Strength){
+            char tempPokerCards[1][20];
+            memcpy(tempPokerCards[0], AllHands[i].hand[0], sizeof(AllHands[i].hand[0]));
+            memcpy(AllHands[i].hand[0], AllHands[i].hand[1], sizeof(AllHands[i].hand[0]));
+            memcpy(AllHands[i].hand[1], tempPokerCards[0], sizeof(AllHands[i].hand[0]));
+        }
     }
     
 }
-void setStrengthOfHand(){
+void setStrengthOfHand(struct hands * AllHands){
     /// complete rest of cards
     
     for(int i =0;i<Handset;i++){
@@ -124,121 +127,125 @@ void setStrengthOfHand(){
             
         }
     }
-
+    
 }
-void SetHands(){
+void SetHands(struct hands *AllHands){
     strcat(AllHands[0].hand[0], "K");
     strcat(AllHands[0].hand[1], "Q");
     
-    strcat(AllHands[1].hand[0], "8");
-    strcat(AllHands[1].hand[1], "A");
+    //strcat(AllHands[1]->hand[0], "8");
+   // strcat(AllHands[1]->hand[1], "A");
+     //printf("%s",AllHands[1]->hand[0]);
+    AllHands[1].hand[0][0]='A';
+    AllHands[1].hand[1][0]='8';
+    // printf("%s",AllHands[1]->hand[1]);
 }
 
 
-void getRandomNumbers(){
+void getRandomNumbers(pokerutil * util){
     
     for(int i = 0; i<200; i++){
-        randomNumber[i]= rand()% (52-(2*Handset)) + 2*Handset;
-        randomNumbers2[i] = rand()% (52-(2*Handset)) + 2*Handset;
+        util->randomNumber[i]= rand()% (52-(2*Handset)) + 2*Handset;
+        util->randomNumbers2[i] = rand()% (52-(2*Handset)) + 2*Handset;
     }
-    
 }
 
 
-void harts(){
+void harts(pokerutil * util){
     char card[8];
     for(int i=2;i<10;i++){
         sprintf(card, "%d\u2665", i);
-        strcpy(pokerCards[i-2], card);
+        strcpy(util->pokerCards[i-2], card);
     }
-    strcpy(pokerCards[8], "T\u2665");
-    strcpy(pokerCards[9], "J\u2665");
-    strcpy(pokerCards[10], "Q\u2665");
-    strcpy(pokerCards[11], "K\u2665");
-    strcpy(pokerCards[12], "A\u2665");
+    strcpy(util->pokerCards[8], "T\u2665");
+    strcpy(util->pokerCards[9], "J\u2665");
+    strcpy(util->pokerCards[10], "Q\u2665");
+    strcpy(util->pokerCards[11], "K\u2665");
+    strcpy(util->pokerCards[12], "A\u2665");
 }
 
 
-void clubs(){
+void clubs(pokerutil * util){
     char card[8];
     for(int i=2;i<10;i++){
         sprintf(card, "%d\u2660", i);
-        strcpy(pokerCards[i+11], card);
+        strcpy(util->pokerCards[i+11], card);
     }
-    strcpy(pokerCards[21], "T\u2660");
-    strcpy(pokerCards[22], "J\u2660");
-    strcpy(pokerCards[23], "Q\u2660");
-    strcpy(pokerCards[24], "K\u2660");
-    strcpy(pokerCards[25], "A\u2660");
+    strcpy(util->pokerCards[21], "T\u2660");
+    strcpy(util->pokerCards[22], "J\u2660");
+    strcpy(util->pokerCards[23], "Q\u2660");
+    strcpy(util->pokerCards[24], "K\u2660");
+    strcpy(util->pokerCards[25], "A\u2660");
     
 }
 
 
-void diamonds(){
+void diamonds(pokerutil * util){
     char card[8];
     for(int i=2;i<10;i++){
         sprintf(card, "%d\u2666", i);
-        strcpy(pokerCards[i+24], card);
+        strcpy(util->pokerCards[i+24], card);
     }
-    strcpy(pokerCards[34], "T\u2666");
-    strcpy(pokerCards[35], "J\u2666");
-    strcpy(pokerCards[36], "Q\u2666");
-    strcpy(pokerCards[37], "K\u2666");
-    strcpy(pokerCards[38], "A\u2666");
+    strcpy(util->pokerCards[34], "T\u2666");
+    strcpy(util->pokerCards[35], "J\u2666");
+    strcpy(util->pokerCards[36], "Q\u2666");
+    strcpy(util->pokerCards[37], "K\u2666");
+    strcpy(util->pokerCards[38], "A\u2666");
 }
 
 
-void spades(){
+void spades(pokerutil * util){
     char card[8];
     for(int i=2;i<10;i++){
         sprintf(card, "%d\u2663", i);
-        strcpy(pokerCards[i+37], card);
+        strcpy(util->pokerCards[i+37], card);
     }
-    strcpy(pokerCards[47], "T\u2663 ");
-    strcpy(pokerCards[48], "J\u2663");
-    strcpy(pokerCards[49], "Q\u2663");
-    strcpy(pokerCards[50], "K\u2663");
-    strcpy(pokerCards[51], "A\u2663");
+    strcpy(util->pokerCards[47], "T\u2663 ");
+    strcpy(util->pokerCards[48], "J\u2663");
+    strcpy(util->pokerCards[49], "Q\u2663");
+    strcpy(util->pokerCards[50], "K\u2663");
+    strcpy(util->pokerCards[51], "A\u2663");
     
 }
 
-int PocketPair(hands CurrentHand){
+int PocketPair(struct hands * AllHands){
     if(AllHands[0].hand[0][0]== AllHands[0].hand[1][0]){
         return 1;
     }
     return 0;
 }
 
-int CommunityCardPair(){
+int CommunityCardPair(pokerutil * util){
     for(int j =(NumPlayers*2) ; j<NumPlayers*2+3;j++){
         for(int i=j ; i<NumPlayers*2+2;i++){
-            if(pokerCards[j][0]==pokerCards[i+1][0]){
+            if(util->pokerCards[j][0]==util->pokerCards[i+1][0]){
                 return 1;
             }
         }
     }
     return 0;
 }
-void createDeck(){
-    harts();
-    clubs();
-    diamonds();
-    spades();
+void createDeck(pokerutil *util){
+    harts(util);
+    clubs(util);
+    diamonds(util);
+    spades(util);
 }
-void shuffle_deck(){
-    getRandomNumbers();
+
+void shuffle_deck(pokerutil * util){
+    getRandomNumbers(util);
     for(int i=0;i<200;i++){
-        replace(randomNumbers2[i], randomNumber[i]);
+        replace(util->randomNumbers2[i], util->randomNumber[i], util);
     }
 }
 
-void DealHands( ){
+void DealHands(pokerutil * util, struct hands * AllHands){
     int pos;
     for(int j=1; j<(2*Handset)+1;j++){
         /// pos moves to the next hand
         pos =(int)(ceil((double)j/2)-1.0);
         for(int i=j; i<52;i++){
-            if(pokerCards[i][0] == AllHands[pos].hand[(j-1)%2][0]){replace(j-1, i); break;}
+            if(util->pokerCards[i][0] == AllHands[pos].hand[(j-1)%2][0]){replace(j-1, i,util); break;}
         }
         
     }
@@ -277,10 +284,11 @@ void isFullHouse(hands hand, int firstCard, int secondCard){
 /////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////// pair logic //////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
-/*
- * NumPlayers*2 skips that many postions in the deck +N is how many cards are seen
- */
+
+ // NumPlayers*2 skips that many postions in the deck +N is how many cards are seen
+ 
 // how many times matches accured.
+
 void typeOfPair(hands hand,int pair, int nextCard){
     
     if(pair==1){
@@ -292,14 +300,14 @@ void typeOfPair(hands hand,int pair, int nextCard){
     }
 }
 
-void findPair(hands hand){
+void findPair(hands hand,pokerutil * util){
     int pair=0;
     int firstCard = 0;
     int secondCard=0;
     /// iterates through a players hand and compares it with the community cards
     for(int nextCard=0;nextCard<2;nextCard++){
         for(int i = NumPlayers*2; i<(NumPlayers*2)+cardsDelt;i++){
-            if(pokerCards[i][0] == hand->hand[nextCard][0]){
+            if(util->pokerCards[i][0] == hand->hand[nextCard][0]){
                 pair++;
             }
         }
@@ -314,16 +322,16 @@ void findPair(hands hand){
     }
 }
 
-void PocketPairCalculations(hands hand){
+void PocketPairCalculations(hands hand,pokerutil * util){
     int pair = 0;
     for(int i = NumPlayers*2; i<(NumPlayers*2)+cardsDelt;i++){
-        if(pokerCards[i][0] == hand->hand[0][0]){
+        if(util->pokerCards[i][0] == hand->hand[0][0]){
             pair++;
         }
     }
     if(pair == 1){
         hand->tripes++;
-        if(CommunityCardPair()){
+        if(CommunityCardPair(util)){
             hand->fullHouse++;
         }
     }else if(pair == 2){
@@ -331,11 +339,11 @@ void PocketPairCalculations(hands hand){
     }
 }
 
-void handAsAnyPairOrFullHouse(int currentHand){
-    if(PocketPair(&AllHands[0])){
-        PocketPairCalculations(&AllHands[currentHand]);
+void handAsAnyPairOrFullHouse(int currentHand, struct hands * AllHands,pokerutil * util){
+    if(PocketPair(AllHands)){
+        PocketPairCalculations(&AllHands[currentHand],util);
     }else{
-        findPair(&AllHands[currentHand]);
+        findPair(&AllHands[currentHand],util);
     }
 }
 
@@ -343,24 +351,24 @@ void handAsAnyPairOrFullHouse(int currentHand){
 ///////////////////////////////////Straight logic////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
 
-int* getCommunityCardNumberValue(){
+int* getCommunityCardNumberValue(pokerutil * util){
     int static communityCards[5];
     for(int i=NumPlayers*2;i<NumPlayers*2+cardsDelt;i++ ){
-        if(pokerCards[i][0]== 'T'){
+        if(util->pokerCards[i][0]== 'T'){
             communityCards[i-NumPlayers*2] = 10;
-        }else if (pokerCards[i][0]== 'J'){
+        }else if (util->pokerCards[i][0]== 'J'){
             communityCards[i-NumPlayers*2] = 11;
-        }else if (pokerCards[i][0]=='Q'){
+        }else if (util->pokerCards[i][0]=='Q'){
             communityCards[i-NumPlayers*2]=12;
-        }else if(pokerCards[i][0]=='K'){
+        }else if(util->pokerCards[i][0]=='K'){
             communityCards[i-NumPlayers*2] = 13;
-        }else if(pokerCards[i][0] == 'A'){
+        }else if(util->pokerCards[i][0] == 'A'){
             communityCards[i-NumPlayers*2] = 14;
         }
         else{
-            communityCards[i-NumPlayers*2] = atoi(&pokerCards[i][0]);
+            communityCards[i-NumPlayers*2] = atoi(&util->pokerCards[i][0]);
         }
-      
+        
     }
     
     return communityCards;
@@ -368,12 +376,12 @@ int* getCommunityCardNumberValue(){
 
 
 
-void Straight(hands hand){
+void Straight(hands hand,pokerutil * util){
     int *cards;
     int count=0;
     int Playercard1 = atoi(hand->hand[1]);
-    cards = getCommunityCardNumberValue();
-  
+    cards = getCommunityCardNumberValue(util);
+    
     for(int j = Playercard1+1;j<Playercard1+6;j++){
         for(int i=0;i<5;i++){
             if(j == cards[i] || atoi(hand->hand[0])==j){
@@ -408,30 +416,18 @@ void Straight(hands hand){
     if (tempCount+count > 3){
         hand->straight++;
     }
-   
+    
 }
 
-void showStrengthOfHand(){
+void showStrengthOfHand(struct hands * AllHands){
     for(int i =0;i<Handset;i++){
         for(int j =0;j<2;j++){
             printf("%d\n",AllHands[i].results[j]->Strength);
         }
     }
 }
-int bestHand2(){
-    int winning = -100;
-    for(int i =0 ; i<Handset;i++){
-        if(AllHands[i].results[0]->pair==1 && AllHands[i].results[0]->Strength>winning ){
-            winning = AllHands[i].results[0]->Strength;
-             printf("%d\n",winning);
-            displayFlop();
-            return 1;
-            
-        }
-    }
-    return 0;
-}
-void setHandToZero(){
+
+void setHandToZero(struct hands * AllHands){
     for(int i=0;i<Handset;i++){
         AllHands[i].besthand.card1=0;
         AllHands[i].besthand.card2=0;
@@ -441,60 +437,67 @@ void setHandToZero(){
     }
 }
 
-void BestPair(){
+void BestPair(struct hands * AllHands,pokerutil * util){
     int currentBestPair=0;
     for(int i=1; i<Handset;i++){
         if(AllHands[i].besthand.pair > 0 && AllHands[i].besthand.pair >= AllHands[currentBestPair].besthand.pair){
             /// update: depends if the kicker plays
-             if(AllHands[i].besthand.pair == AllHands[currentBestPair].besthand.pair){
-                 if(AllHands[i].besthand.kicker > AllHands[currentBestPair].besthand.kicker){
-                     currentBestPair =i;
-                 }
-             }else{
-                 currentBestPair = i;
-             }
-        
+            if(AllHands[i].besthand.pair == AllHands[currentBestPair].besthand.pair){
+                if(AllHands[i].besthand.kicker > AllHands[currentBestPair].besthand.kicker){
+                    currentBestPair =i;
+                }
+            }else{
+                currentBestPair = i;
+            }
+            
         }
     }
+    if(AllHands[currentBestPair].besthand.pair > 0){
     printf("Top pair %d and Kicker %d\n",AllHands[currentBestPair].besthand.pair,AllHands[currentBestPair].besthand.kicker);
-    displayFlop();
-    setHandToZero();
+    displayFlop(util);
+    }
+    setHandToZero(AllHands);
 }
 void bestTwoPair(){
-
+    
 }
-void StatsOfAllIterations(){
+void StatsOfAllIterations(struct hands * AllHands){
     printf("odds of getting pair on the flop %f\n",(float)AllHands[0].pair/1000000);
     printf("odds of getting 2 pair on the flop %f\n",(float)AllHands[0].twopair/1000000);
     printf("odds of getting tripes on the flop %f\n",(float)AllHands[0].tripes/1000000);
 }
+void startSimulation(pokerutil * util, struct hands * AllHands){
+    for(int i =0 ;i<100000;i++){
+        for(int j=0; j<Handset;j++){
+            handAsAnyPairOrFullHouse(j,AllHands,util);
+        }
+        BestPair(AllHands,util);
+        shuffle_deck(util);
+    }
+    StatsOfAllIterations(AllHands);
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     //shouldbreak();
+    struct hands AllHands[26];
+    struct pokerUtil util;
     srand((unsigned int )time(NULL));
-    SetHands();
-    createDeck();
-    DealHands( );
-    setStrengthOfHand();
-    higherOrderFirst();
-    setStrengthOfHand();
-  //  displaycards();
-    //printf("\n\n\nlu\n\n\n",strlen(AutoSetHands));
-    //v3 = rand() % 30 + 1985;   // v3 in the range 1985-2014
+    SetHands(AllHands);
+    createDeck(&util);
+    DealHands(&util,AllHands);
     
-    shuffle_deck();
-        for(int i =0 ;i<100000;i++){
-            for(int j=0; j<Handset;j++){
-            handAsAnyPairOrFullHouse(j);
-            }
-            BestPair();
-           // bestTwoPair();
-            shuffle_deck();
-        }
-    StatsOfAllIterations();
-    displaycards();
+    setStrengthOfHand(AllHands);
+    higherOrderFirst(AllHands);
+    setStrengthOfHand(AllHands);
+    
+    displaycards(&util);
+    shuffle_deck(&util);
+    startSimulation(&util,AllHands);
+    //test(&AllHands);
+    printf("%d\n", AllHands[1].pair);
    
     return 0;
 }
 //12006 raintree drive
 //245 sherman ave
+
